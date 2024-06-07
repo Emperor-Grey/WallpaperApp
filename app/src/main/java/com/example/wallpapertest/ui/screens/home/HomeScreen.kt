b966aebf03ed760d2575b6f1961ec83ceb473877
King Grey <lohitsaidev@gmail.com>
2024-06-07 15:34:43 +0530

    Added Fab

--- app/src/main/java/com/example/wallpapertest/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/wallpapertest/ui/screens/home/HomeScreen.kt
@@ -1,20 +1,31 @@
 package com.example.wallpapertest.ui.screens.home
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
     Scaffold(topBar = {
         HomeTopAppBar()
+    }, floatingActionButton = {
+        SearchFab()
     }, containerColor = MaterialTheme.colorScheme.background) {
         HomeContent(modifier = Modifier.padding(it))
+    }
+}
+
+@Composable
+fun SearchFab() {
+    FloatingActionButton(onClick = { /*TODO*/ }) {
+        Icon(imageVector = Icons.Filled.Search, contentDescription = "Search Icon")
     }
 }
 
