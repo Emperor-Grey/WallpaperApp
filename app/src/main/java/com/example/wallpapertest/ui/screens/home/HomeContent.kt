af65f746c026f8913e342fc2a4b17a31496d1d25
King Grey <lohitsaidev@gmail.com>
2024-06-08 11:06:55 +0530

    Added Crash mechanism (bug)

--- app/src/main/java/com/example/wallpapertest/ui/screens/home/HomeContent.kt
+++ app/src/main/java/com/example/wallpapertest/ui/screens/home/HomeContent.kt
@@ -1,16 +1,22 @@
 package com.example.wallpapertest.ui.screens.home
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,39 +26,42 @@ import com.example.wallpapertest.components.ScrollTab
 import com.example.wallpapertest.components.WallGrid
 import com.example.wallpapertest.ui.theme.salsaFontFamily
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeContent(modifier: Modifier) {
-
     val categories = listOf("Random", "Popular", "Featured", "Anime", "Nature")
     val selectedIndex = remember { mutableIntStateOf(0) }
 
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-//            .verticalScroll(rememberScrollState()) // remove the comment to see the error
-            .padding(16.dp)
+    LazyColumn(
+        modifier = modifier.fillMaxSize()
     ) {
-        AICard()
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(
-            text = "Discover",
-            fontFamily = salsaFontFamily,
-            fontWeight = FontWeight.Medium,
-            style = MaterialTheme.typography.headlineLarge,
-            color = MaterialTheme.colorScheme.onSurface
-        )
-        Text(
-            text = "Discover the best wallpapers",
-            fontFamily = salsaFontFamily,
-            style = MaterialTheme.typography.titleMedium.copy(fontSize = 18.sp),
-            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
-        )
-        Spacer(modifier = Modifier.height(12.dp))
-        ScrollTab(categories = categories, selectedIndex = selectedIndex.intValue) {
-            selectedIndex.intValue = it
+        item {
+            AICard()
+            Spacer(modifier = Modifier.height(16.dp))
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        WallGrid()
+
+        item(stickyHeader {
+            Text(
+                text = "Discover",
+                fontFamily = salsaFontFamily,
+                fontWeight = FontWeight.Medium,
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Text(
+                text = "Discover the best wallpapers",
+                fontFamily = salsaFontFamily,
+                style = MaterialTheme.typography.titleMedium.copy(fontSize = 18.sp),
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            ScrollTab(categories = categories, selectedIndex = selectedIndex.intValue) {
+                selectedIndex.intValue = it
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }) {
+           WallGrid()
+        }
     }
 }
 
